81d78401d52ba2a40b16d916878f9e6112defeb0
Олександр Козак <kozackunisoft@gmail.com>
2024-01-21 14:59:04 +0200

    адаптив

--- src/_includes/base.jsx
+++ src/_includes/base.jsx
@@ -3,12 +3,14 @@ export default ({title, children}) => (<html>
     <title>{title}</title>
     <link rel="stylesheet" href="/styles.css"/>
 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
 </head>
 <body>
-    <div id={'wrapper'}>
+<div id={'wrapper'}>
 
-        {children}
+    {children}
 
-    </div>
+</div>
 </body>
 </html>);
