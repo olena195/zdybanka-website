038ed00e23123005312a194c338f32624942bca7
Олександр Козак <kozackunisoft@gmail.com>
2024-01-21 16:56:50 +0200

    netlify redirect

--- src/_includes/base.jsx
+++ src/_includes/base.jsx
@@ -1,7 +1,7 @@
 export default ({title, children}) => (<html lang={'uk'}>
 <head>
     <title>{title}</title>
-    <link rel="stylesheet" href="/styles.css"/>
+    <link rel="stylesheet" href="/assets/styles/styles.css"/>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
