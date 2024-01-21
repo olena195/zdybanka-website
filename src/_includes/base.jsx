1cab6ebcecfdcc3a95eb410bcbdd480089897b09
Олександр Козак <kozackunisoft@gmail.com>
2024-01-21 17:37:50 +0200

    google-site-verification

--- src/_includes/base.jsx
+++ src/_includes/base.jsx
@@ -6,6 +6,7 @@ export default ({title, children}) => (<html lang={'uk'}>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
     <meta name="color-scheme" content="dark"/>
+    <meta name="google-site-verification" content="-RbpAq2XwaLvBfAOCxCPMKjVDFImY6GKTqsdsQTUKpg" />
 
 </head>
 <body>
