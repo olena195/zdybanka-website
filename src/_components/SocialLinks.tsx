d66211ba0ed266a1cb44678f3f776ab2d93c21f8
Олександр Козак <kozackunisoft@gmail.com>
2024-02-16 19:15:25 +0200

    bmc

--- src/_components/SocialLinks.tsx
+++ src/_components/SocialLinks.tsx
@@ -1,8 +1,8 @@
 export default () => (
         <ul className={'flex flex-wrap gap-6 justify-center'}>
-            <li><a href="#">Twitter</a></li>
-            <li><a href="#">Twitter</a></li>
-            <li><a href="#">Twitter</a></li>
-            <li><a href="#">Twitter</a></li>
+            {/*<li><a href="#">Twitter</a></li>*/}
+            {/*<li><a href="#">Twitter</a></li>*/}
+            {/*<li><a href="#">Twitter</a></li>*/}
+            {/*<li><a href="#">Twitter</a></li>*/}
         </ul>
 )
